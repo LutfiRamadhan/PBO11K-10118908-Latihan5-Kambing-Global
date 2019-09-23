7a098f7099e966b45cd265beefe865c2784a8ade
Lutfi Ramadhan <lutfiramadan@gmail.com>
2019-09-23 22:37:16 +0700

    First Commit

---
+++ src/latihan5/Latihan5.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan5;
+
+/**
+ *
+ * @author lutfi
+ * NAMA  : LUTFI RAMADHAN
+ * NIM   : 10118908
+ * KELAS : IF 11 Karyawan
+ */
+public class Latihan5 {
+    
+    int jumlahKambing = 88;
+    
+    public void getJumlahKambing(){
+        System.out.println("Jumlah Kambing "+jumlahKambing);
+    }
+    
+    public void tambahKambing(){
+        jumlahKambing = jumlahKambing+5;
+        System.out.println("Jumlah Kambing setelah ditambah : "+jumlahKambing);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Latihan5 kambingSusu = new Latihan5();
+        kambingSusu.tambahKambing();
+        kambingSusu.getJumlahKambing();
+    }
+    
+}
